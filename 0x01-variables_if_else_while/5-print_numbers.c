6d5a0c2d52e35367ecf703786e73f9017c9c01cd
MwangiN <mwangitm1996@gmail.com>
2022-07-01 21:40:45 +0300

    print numbers to base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 0; p <= 9; p++)
+		printf("%i", p);
+	putchar('\n');
+	return (0);
+}
