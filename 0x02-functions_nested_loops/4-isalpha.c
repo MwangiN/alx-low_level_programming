49eb9018fdd42ee4cb2081cfd55279123efd9ca6
MwangiN <mwangitm1996@gmail.com>
2022-07-05 17:46:47 +0300

    Changes to main.h

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
- #include "main.h"
+#include "main.h"
 /**
  *_isalpha - checks for lowercase and uppercase character
  * @c: ASCII code
@@ -6,7 +6,7 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' && c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
