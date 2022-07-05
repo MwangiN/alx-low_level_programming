981a065e340706e1ad52e0ec8c65ba7d2f146285
MwangiN <mwangitm1996@gmail.com>
2022-07-06 00:02:17 +0300

    fibonacci sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print fibonacci
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	unsigned int x = 1, y = 2, z = 3, v = 2, s;
+
+	for (s = 2; s <= 32; s++)
+	{
+		if (z % 2 == 0)
+			v = v + z;
+		x = y;
+		y = z;
+		z = x + y;
+	}
+	printf("%u\n", v);
+	return (0);
+}
