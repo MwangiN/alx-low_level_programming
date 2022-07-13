444f2419be3fbe0b65742df359c133724492279e
MwangiN <mwangitm1996@gmail.com>
2022-07-13 23:10:14 +0300

    strncpy strcmp

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,5 +4,7 @@
 int _putchar(char c);
 char *_strncat(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);i
+int _strcmp(char *s1, char *s2);
 
 #endif
